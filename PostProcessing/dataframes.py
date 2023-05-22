5d588ebd3f4ab48be2c8ff94a4d109b044f3b0d5
Fabrizio Nunnari <fab.nunnari@gmail.com>
2023-05-22 23:36:44 +0200

    More comments.

--- PostProcessing/dataframes.py
+++ PostProcessing/dataframes.py
@@ -18,17 +18,21 @@ def compute_time_step(video_timestamps: pd.DataFrame) -> float:
     """
 
     first_col_name = video_timestamps.columns[0]
+    # Retrieves the most frequent time different between consecutive lines.
     time_step = (video_timestamps[first_col_name].diff()).dropna().value_counts().index[0]
 
     return time_step
 
 
-def repair_dropped_frames(df: pd.DataFrame,  time_step: float) -> pd.DataFrame:
-
+def repair_dropped_frames(df: pd.DataFrame, time_step: float) -> pd.DataFrame:
+    # The name of the first column (can be anythign as the original df doesn't have header
     first_col_name = df.columns[0]
 
+    # Forces the type of the timestamps to int64
     df[first_col_name] = pd.to_datetime(df[first_col_name]).astype(np.int64)
+    # Retrieves the timestamps into a Serie
     timestamps = df[first_col_name]
+    # Will accumulate the repaired rows
     repaired_rows = []
 
     # Check for missing timestamps and generate them
@@ -36,23 +40,28 @@ def repair_dropped_frames(df: pd.DataFrame,  time_step: float) -> pd.DataFrame:
         timestamp = timestamps.iloc[i]
         next_timestamp = timestamps.iloc[i + 1]
 
+        # The current timestamp is by definition original
         repaired_rows.append([timestamp, 'Original'])
 
+        # If the next timestamp exceeds the expected time step
         if next_timestamp - timestamp > time_step:
-            missing_timestamps_count = int((next_timestamp - timestamp)/ time_step) - 1
+            # Estimate the number of missing frames
+            missing_timestamps_count = int((next_timestamp - timestamp) / time_step) - 1
+            # Estimate a time interval between them (will be very similar to the input time_step
             interval = (next_timestamp - timestamp) / (missing_timestamps_count + 1)
-
+            # Generate the missing lines
             for j in range(1, missing_timestamps_count + 1):
                 new_timestamp = (timestamp + j * interval).astype(np.int64)
                 repaired_rows.append([new_timestamp, 'Generated'])
 
     # Add the last row
     repaired_rows.append([timestamps.iloc[-1], 'Original'])
+    # print(len(repaired_rows))
 
-    print(len(repaired_rows))
     # Create a new DataFrame with repaired rows
     columns = ['timestamp', 'generated']
     output_df = pd.DataFrame(repaired_rows, columns=columns)
+    # Forces the output timestamp type to int 64
     output_df['timestamp'] = pd.to_datetime(output_df['timestamp']).astype(np.int64)
 
     return output_df
